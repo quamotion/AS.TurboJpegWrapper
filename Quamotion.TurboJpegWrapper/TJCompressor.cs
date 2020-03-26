f67cf8e6ceb3999105c307d0c25de0f3a7bc3bda
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-03-26 19:30:25 +0100

    TJCompressor :Add Span<byte> and byte[] overloads

--- Quamotion.TurboJpegWrapper/TJCompressor.cs
+++ Quamotion.TurboJpegWrapper/TJCompressor.cs
@@ -255,6 +255,157 @@ namespace TurboJpegWrapper
         }
 
         /// <summary>
+        /// Compresses input image to the jpeg format with specified quality.
+        /// </summary>
+        /// <param name="srcBuf">
+        /// Image buffer containing RGB, grayscale, or CMYK pixels to be compressed.
+        /// This buffer is not modified.
+        /// </param>
+        /// <param name="destBuf">
+        /// A <see cref="byte"/> array containing the compressed image.
+        /// </param>
+        /// <param name="stride">
+        /// Bytes per line in the source image.
+        /// Normally, this should be <c>width * BytesPerPixel</c> if the image is unpadded,
+        /// or <c>TJPAD(width * BytesPerPixel</c> if each line of the image
+        /// is padded to the nearest 32-bit boundary, as is the case for Windows bitmaps.
+        /// You can also be clever and use this parameter to skip lines, etc.
+        /// Setting this parameter to 0 is the equivalent of setting it to
+        /// <c>width * BytesPerPixel</c>.
+        /// </param>
+        /// <param name="width">Width (in pixels) of the source image.</param>
+        /// <param name="height">Height (in pixels) of the source image.</param>
+        /// <param name="pixelFormat">Pixel format of the source image (see <see cref="PixelFormat"/> "Pixel formats").</param>
+        /// <param name="subSamp">
+        /// The level of chrominance subsampling to be used when
+        /// generating the JPEG image (see <see cref="TJSubsamplingOption"/> "Chrominance subsampling options".)
+        /// </param>
+        /// <param name="quality">The image quality of the generated JPEG image (1 = worst, 100 = best).</param>
+        /// <param name="flags">The bitwise OR of one or more of the <see cref="TJFlags"/> "flags".</param>
+        /// <returns>
+        /// The size of the JPEG image (in bytes).
+        /// </returns>
+        /// <exception cref="TJException">
+        /// Throws if compress function failed.
+        /// </exception>
+        /// <exception cref="ObjectDisposedException">Object is disposed and can not be used anymore.</exception>
+        /// <exception cref="NotSupportedException">
+        /// Some parameters' values are incompatible:
+        /// <list type="bullet">
+        /// <item><description>Subsampling not equals to <see cref="TJSubsamplingOption.Gray"/> and pixel format <see cref="TJPixelFormat.Gray"/></description></item>
+        /// </list>
+        /// </exception>
+        public unsafe int Compress(byte[] srcBuf, byte[] destBuf, int stride, int width, int height, PixelFormat pixelFormat, TJSubsamplingOption subSamp, int quality, TJFlags flags)
+        {
+            return this.Compress(srcBuf.AsSpan(), destBuf.AsSpan(), stride, width, height, pixelFormat, subSamp, quality, flags).Length;
+        }
+
+        /// <summary>
+        /// Compresses input image to the jpeg format with specified quality.
+        /// </summary>
+        /// <param name="srcBuf">
+        /// Image buffer containing RGB, grayscale, or CMYK pixels to be compressed.
+        /// This buffer is not modified.
+        /// </param>
+        /// <param name="destBuf">
+        /// A <see cref="byte"/> array containing the compressed image.
+        /// </param>
+        /// <param name="stride">
+        /// Bytes per line in the source image.
+        /// Normally, this should be <c>width * BytesPerPixel</c> if the image is unpadded,
+        /// or <c>TJPAD(width * BytesPerPixel</c> if each line of the image
+        /// is padded to the nearest 32-bit boundary, as is the case for Windows bitmaps.
+        /// You can also be clever and use this parameter to skip lines, etc.
+        /// Setting this parameter to 0 is the equivalent of setting it to
+        /// <c>width * BytesPerPixel</c>.
+        /// </param>
+        /// <param name="width">Width (in pixels) of the source image.</param>
+        /// <param name="height">Height (in pixels) of the source image.</param>
+        /// <param name="pixelFormat">Pixel format of the source image (see <see cref="PixelFormat"/> "Pixel formats").</param>
+        /// <param name="subSamp">
+        /// The level of chrominance subsampling to be used when
+        /// generating the JPEG image (see <see cref="TJSubsamplingOption"/> "Chrominance subsampling options".)
+        /// </param>
+        /// <param name="quality">The image quality of the generated JPEG image (1 = worst, 100 = best).</param>
+        /// <param name="flags">The bitwise OR of one or more of the <see cref="TJFlags"/> "flags".</param>
+        /// <returns>
+        /// A <see cref="Span{T}"/> which is a slice of <paramref name="destBuf"/> which holds the compressed image.
+        /// </returns>
+        /// <exception cref="TJException">
+        /// Throws if compress function failed.
+        /// </exception>
+        /// <exception cref="ObjectDisposedException">Object is disposed and can not be used anymore.</exception>
+        /// <exception cref="NotSupportedException">
+        /// Some parameters' values are incompatible:
+        /// <list type="bullet">
+        /// <item><description>Subsampling not equals to <see cref="TJSubsamplingOption.Gray"/> and pixel format <see cref="TJPixelFormat.Gray"/></description></item>
+        /// </list>
+        /// </exception>
+        public unsafe Span<byte> Compress(Span<byte> srcBuf, Span<byte> destBuf, int stride, int width, int height, PixelFormat pixelFormat, TJSubsamplingOption subSamp, int quality, TJFlags flags)
+        {
+            if (this.isDisposed)
+            {
+                throw new ObjectDisposedException(nameof(TJCompressor));
+            }
+
+            var tjPixelFormat = TJUtils.ConvertPixelFormat(pixelFormat);
+            CheckOptionsCompatibilityAndThrow(subSamp, tjPixelFormat);
+
+            ulong destBufSize = (ulong)destBuf.Length;
+
+            fixed (byte* srcBufPtr = srcBuf)
+            fixed (byte* destBufPtr = destBuf)
+            {
+                IntPtr destBufPtr2 = (IntPtr)destBufPtr;
+
+                var result = TurboJpegImport.TjCompress2(
+                    this.compressorHandle,
+                    (IntPtr)srcBufPtr,
+                    width,
+                    stride,
+                    height,
+                    (int)tjPixelFormat,
+                    ref destBufPtr2,
+                    ref destBufSize,
+                    (int)subSamp,
+                    quality,
+                    (int)flags);
+
+                if (result == -1)
+                {
+                    TJUtils.GetErrorAndThrow();
+                }
+            }
+
+            return destBuf.Slice(0, (int)destBufSize);
+        }
+
+        /// <summary>
+        /// The maximum size of the buffer (in bytes) required to hold a JPEG image with
+        /// the given parameters.  The number of bytes returned by this function is
+        /// larger than the size of the uncompressed source image.  The reason for this
+        /// is that the JPEG format uses 16-bit coefficients, and it is thus possible
+        /// for a very high-quality JPEG image with very high-frequency content to
+        /// expand rather than compress when converted to the JPEG format.  Such images
+        /// represent a very rare corner case, but since there is no way to predict the
+        /// size of a JPEG image prior to compression, the corner case has to be handled.
+        /// </summary>
+        /// <param name="width">Width (in pixels) of the image.</param>
+        /// <param name="height">Height (in pixels) of the image.</param>
+        /// <param name="subSamp">
+        /// The level of chrominance subsampling to be used when
+        /// generating the JPEG image(see <see cref="TJSubsamplingOption"/> "Chrominance subsampling options".)
+        /// </param>
+        /// <returns>
+        /// The maximum size of the buffer (in bytes) required to hold the image,
+        /// or -1 if the arguments are out of bounds.
+        /// </returns>
+        public int GetBufferSize(int width, int height, TJSubsamplingOption subSamp)
+        {
+            return (int)TurboJpegImport.TjBufSize(width, height, (int)subSamp);
+        }
+
+        /// <summary>
         /// Releases resources.
         /// </summary>
         /// <filterpriority>2.</filterpriority>
